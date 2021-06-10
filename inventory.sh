7ce4b95fa286a4a8b931e1cd3d55ae99fbe28748
Munirih Nganyade <linkmunirih@gmail.com>
2021-06-10 11:25:33 +0200

    third update.1

--- inventory.sh
+++ inventory.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-
 ##Modified by: linkmunirih@gmail.com
 ##Date: 09-06-2021
 ##Description: This script is created to do a full inventory check based on what the user requires.
 
-os_ver=`cat /etc/*release`
+os_ver=`cat /etc/*release | awk 'NR==1 {print}'`
 echo "the OS version is ${os_ver}"
 sleep 2
 
 echo "the total number of CPU in this system is `nproc`"
 sleep 2
 
-echo "the harddrive size is `lsblk`"
+echo -e "the harddrive size is \n `lsblk`"
 sleep 2
 
 scpu=`cat /proc/cpuinfo |awk '/cpu MHz/ {print $4}'`
@@ -33,23 +32,27 @@ ipadd=`ifconfig |grep inet |awk 'NR==1 {print $2}'`
 echo "This system's IP address is ${ipadd}"
 
 p=`firewall-cmd --list-ports`
-echo -e "the list of this  system's opened ports \n ${p}"
+echo -e "the list of all opened ports on this system is as follows: \n ${p}"
+sleep 2
+
+dn=`cat /etc/resolv.conf | grep -i nameserver | awk '{print $2}'`
+echo "The system's DNS is ${dn} "
 sleep 2
 
 manu=`dmidecode -t system| grep -i manufacturer |awk '{print $2, $3}'`
 echo "The system's manufacturer is ${manu}"
 sleep 2
 
-if [ ${manu} == innotek Gmbh ] || [ ${manu} == GEMU ]
+if [ ${manu} == 'innotek GmbH' ] || [ ${manu} == 'QEMU' ]
 then
 echo "This system is a virtual."
 else
 echo "This system is physical."
 fi
 sleep 2
-macadd=`cat /sys/class/net/*/address | awk 'NR== 1 {print}'`
-echo "The system's mac address is ${macadd}"
+
+macadd=`cat /sys/class/net/*/address | awk 'NR==1 {print}'`
+echo "the system's MAC address is ${macadd}"
 sleep 2
 echo "end of system inventory check"
-
 
